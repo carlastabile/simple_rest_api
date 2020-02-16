566e5051efdbb6f6c99b63bfa0dd6a7bd66654e6
Carla Urrea Stabile <carla.urrea@xing.com>
2020-02-16 12:28:53 +0100

    Empty models

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -1,11 +1,3 @@
 class Pet < ApplicationRecord
   belongs_to :user
-  # If for some reason we want to rename the association, this is the way to do it
-  # belongs_to :hooman, class_name: "User", foreign_key: :user_id
-
-  # Validate the model fields
-  validates :name, presence: true
-  validates :kind, inclusion: { in: %w(dog cat fish),
-                                message: "%{value} is not a valid pet kind :(" }, allow_nil: true
-  validates :age_in_years, numericality: { greater_than_or_equal_to: 0 }
 end
