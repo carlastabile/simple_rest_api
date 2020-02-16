fca38a1a18071547456d96bc32104783d532ebf8
Carla Urrea Stabile <carla.urrea@xing.com>
2020-02-16 12:39:27 +0100

    Empty users controller and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,4 @@
 Rails.application.routes.draw do
-  namespace :api do
-    namespace :v1 do
-      resources :users, only: [:create, :show, :index] do
-        resources :pets, only: [:index, :create, :destroy, :show]
-      end
-    end
-  end
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
