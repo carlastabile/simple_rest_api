f4061b18e83c2ab8bf136d48b8967978e6510ab0
Carla Urrea Stabile <carla.urrea@xing.com>
2020-02-16 12:41:10 +0100

    Remove create pet spec

--- spec/requests/api/v1/pets_request_spec.rb
+++ spec/requests/api/v1/pets_request_spec.rb
@@ -2,24 +2,7 @@ require 'rails_helper'
 require 'json_helper'
 
 RSpec.describe "Api::V1::Pets", type: :request do
-
-  describe "Create pet" do
-    before(:all) do
-      @user_id = User.create(username: "foo", password: "123456").id
-    end
-
-    it "should be valid" do
-      post "/api/v1/users/#{@user_id}/pets", params: { pet: { name: "dasha", kind: "dog", age_in_years: 1, user_id: @user_id} }
-
-      expect(response).to have_http_status(:no_content)
-    end
-
-    it "fails to create a pet with bad params" do
-      post "/api/v1/users/#{@user_id}/pets", params: { pet: { name: "dasha", kind: "blah", age_in_years: 0, user_id: @user_id} }
-
-      expect(response).to have_http_status(:bad_request)
-    end
-  end
+  
 
   describe "List pets" do
     before(:all) do
